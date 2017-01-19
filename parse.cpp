fe42bfb13be01f52057f4f799aa69e3bacaf9ade
Ryan <ryan.pierce@oit.edu>
2017-01-19 13:18:18 -0800

    Passed the first test

--- parse.cpp
+++ parse.cpp
@@ -14,14 +14,192 @@
 #include "lex.h"
 #include "parse.h"
 #include "utils.h"
-
+#include "tokens.h"
+using std::endl;
+using std::cout;
 //*******************************************
 // Find a PROG non-terminal
 bool FindPROG()
 {
-    Error("Program");
-    return false;
+	if(FindSTMTS() && yylex() != END){
+		return true;
+	}
+	else{
+		return false;
+	}
 }
+bool FindSTMTS()
+{
+	if(FindSTMT() && FindSTMTS())
+	{
+		return true;
+	}
+	return true;
+}
+bool FindSTMT()
+{
+	if(FindEXPR())
+	{
+		if(PeekToken()  == ';') {
+		AdvanceToken();
+		cout << "Found a statement" << endl;
+		return true;
+		}
+		else {
+			Error("';'");
+			while(PeekToken() != ';'){
+				AdvanceToken();
+			}
+			return true;
+		}
+	}
+	else{
+		return false;
+	}
+}
+bool FindEXPR()
+{
+	if(PeekToken() == '(')
+	{
+		AdvanceToken();
+		if (!FindEXPR()) return false;
+		
+		if(PeekToken() == ')')
+		{
+			AdvanceToken();
+		}
+		else {
+			Error("')'");
+			return false;
+		}
+
+		if(!FindEXPR_P()) return false;
+
+		return true; 
+	}
+	else if(FindTERM())
+	{
+		 return true;
+	}
+	else 
+	{
+		return false;
+	}
+}
+
+bool FindEXPR_P()
+{
+	if(FindPLUSOP())
+	{
+		if(PeekToken() == '(')
+		{
+			AdvanceToken();
+		}
+		else
+		{	
+			Error("'(' ");
+			return false;
+		}
+		
+		if(!FindEXPR()) return false;
+		
+		if(PeekToken() == ')')
+		{
+			AdvanceToken();
+		}
+		else
+		{
+			Error("')' ");
+			return false;
+		}
+		if(!FindEXPR_P()) return false;
+	}
+	return true;
+}
+bool FindPLUSOP()
+{
+	if (PeekToken() == PLUSOP)
+	{
+		AdvanceToken();
+		return true;
+	}
+	else
+	{
+		return false;
+	}	
+}
+bool FindTERM()
+{
+	if(PeekToken() == '[')
+	{
+		AdvanceToken();
+
+		if (!FindEXPR()) return false;
+			
+		if (PeekToken() == ']')
+		{
+			AdvanceToken();
+		}
+		else {
+			Error("']'");
+			return false;
+		}
+
+		if (!FindTERM_P()) return false;
+		
+		return true;
+	}
+	else if(PeekToken() == NUM){
+		AdvanceToken();
+		return true;
+	}
+	else {
+		return false;
+	}
+	
+}
+bool FindTERM_P()
+{
+	if(FindTIMESOP()) 
+	{
+		if(PeekToken() == '[')
+		{
+			AdvanceToken();
+		}
+		else
+		{
+			Error("'[' ");
+			return false;
+		}
+		if(!FindEXPR()) return false;
+		
+		if(PeekToken() == ']')
+		{
+			AdvanceToken();
+		}
+		else
+		{
+			Error(" ']' ");
+			return false;
+		}
+		if(!FindTERM_P()) return false;
+	}
+	return true;
+
+}
+bool FindTIMESOP()
+{
+	if(PeekToken() == TIMESOP)
+	{
+		AdvanceToken();
+		return true;
+	}
+	else 
+	{
+		return false;
+	}
+}
+
 /*******************************************
 bool FindExample()
 {
---
+++ tokens.h
@@ -0,0 +1,25 @@
+#pragma once
+
+//**************************************
+// tokens.h
+//
+// Defines the values for the tokens returned by yylex
+//
+// Author: Phil Howard 
+// phil.howard@oit.edu
+//
+// Date: Dec 30, 2015
+//
+
+#define PROG 1000
+#define STMTS 1001
+#define STMT 1002
+#define EXPR 1003
+#define EXPR_P 1004
+#define PLUSOP 1006
+#define TERM 1007
+#define TERM_P 1008
+#define TIMESOP 1009
+#define NUM 1010
+#define JUNK_TOKEN 2000
+#define END 1011
